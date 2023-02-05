9b6d9dff239e7e8052873d70eee561d87348b3ac
Khosraw Azizi <khosraw.azizi@gmail.com>
2023-02-04 20:27:55 -0600

    update window name every 15 seconds

--- script_windows.py
+++ script_windows.py
@@ -2,21 +2,21 @@ import subprocess
 import time
 from pypresence import Presence
 
-process = subprocess.Popen(["powershell.exe", ".\\script.ps1"], stdout=subprocess.PIPE, universal_newlines=True)
-title = process.communicate()[0].strip()
-
 """
 You need to upload your image(s) here:
 https://discordapp.com/developers/applications/<APP ID>/rich-presence/assets
 """
 
-client_id = "861651444234846258" # application ID
+client_id = "1071611439116206101" # application ID
 RPC = Presence(client_id=client_id)
 RPC.connect()
 
-# use the same name that you used when uploading the image
-RPC.update(large_image="notion_1024", large_text="Notion",
-            small_image="keeb_512", small_text="Organizing my life!", start=int(time.time()), details=("Editing: " + title))
-
 while 1:
+    process = subprocess.Popen(["powershell.exe", ".\\script.ps1"], stdout=subprocess.PIPE, universal_newlines=True)
+    title = process.communicate()[0].strip()
+
+    # use the same name that you used when uploading the image
+    RPC.update(large_image="notion_1024", large_text="Notion",
+                small_image="keeb_512", small_text="Organizing my life!", start=int(time.time()), details=("Editing: " + title))
+
     time.sleep(15) #Can only update presence every 15 seconds
