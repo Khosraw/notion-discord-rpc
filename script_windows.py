a8d2ccd3ecde7bdb740f45d7da3257417d064613
Khosraw Azizi <khosraw.azizi@gmail.com>
2023-02-04 20:38:15 -0600

    fixed start time reset after rpc update

--- script_windows.py
+++ script_windows.py
@@ -11,12 +11,14 @@ client_id = "861651444234846258" # application ID
 RPC = Presence(client_id=client_id)
 RPC.connect()
 
+start = int(time.time())
+
 while 1:
     process = subprocess.Popen(["powershell.exe", ".\\script.ps1"], stdout=subprocess.PIPE, universal_newlines=True)
     title = process.communicate()[0].strip()
 
     # use the same name that you used when uploading the image
     RPC.update(large_image="notion_1024", large_text="Notion",
-                small_image="keeb_512", small_text="Organizing my life!", start=int(time.time()), details=("Editing: " + title))
+                small_image="keeb_512", small_text="Organizing my life!", start=int(start), details=("Editing: " + title))
 
     time.sleep(15) #Can only update presence every 15 seconds
